0219a5c47e139c43532946843446e132cab3fabf
wildarch <daandegraaf9@gmail.com>
2017-03-21 01:10:08 +0100

    Make Main more basic

--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,20 @@
-module Main where
-
 import Lib
 import Data.Maybe
 import Data.List
-
-type Average = Float
 
 accessToken :: String
 accessToken = "7542~kMBJqLyi52mSMiP3YyivwzN0SHrHw6GUnraUAhyRTBAl3KcLlwBoStGAYa0z2cta"
 
 main :: IO ()
 main = do
-  -- INPUT
-    readToken <- getToken
-    let token = fromMaybe accessToken readToken
+  courses <- getCourses accessToken
+  let avg = transform courses
+  print avg
 
-    courses <- getCourses token
-
-  -- TRANSFORM
-    let avg = transform courses
-
-    -- OUTPUT
-    list courses
-    display avg
-
-getToken :: IO (Maybe String)
-getToken = do
-    putStrLn "Enter access token:"
-    line <- getLine
-    return $ if line == ""
-      then Nothing
-      else Just line
-
+type Average = Float
 
 transform :: [Course] -> Average
 transform = average . mapMaybe grade
 
-average :: [Float] -> Float
+average :: [Grade] -> Average
 average gs = sum gs / genericLength gs
-
-display :: Average -> IO ()
-display avg = do
-    putStrLn ("Average grade: " ++ show avg ++ "/100")
-    putStrLn (advice avg)
-
-list :: [Course] -> IO ()
-list = mapM_ print
-
-advice :: Average -> String
-advice avg
-    | avg < 55 = "You'd better step it up buddy"
-    | avg > 65 = "Way to go dude!"
-    | otherwise = "You're living on the edge"
